327443c043602c7edbe46c39d2e73a00a04504f7
Matthias Leuffen <matthes@leuffen.de>
2023-07-02 01:45:24 +0200

    update

--- src/Cli/Website.php
+++ src/Cli/Website.php
@@ -41,14 +41,40 @@ class Website
 
         $openAi = $this->brixEnv->getOpenAiApi();
 
-        /*
-        $openAi->addClass(new SingleFileAccessFunctions(
-            fn() => "no data to read",
-            fn($data) => $targetPage->body = $data
 
-        ));
-        */
-        $targetPage->body = $openAi->textComplete("Example output: \"\"\"\n{$example->body}\n\"\"\". Generate output from \"\"\"\n{$this->brixEnv->contextCombined}\n\"\"\". Just write new text. Use exact the same structure and formatting like example.", streamOutput: true);
+        $systemRole = <<<EOT
+
+You create content for a website using example sites. Some information about the owner of the website and the context of the website:
+
+{$this->brixEnv->contextCombined}
+
+Use this information only for reference. Don't use it to create content. It is important to keep the number and order of
+structure of headings, blockquotes and paragraphs. Never edit styling specified in curved brackets {}. Preserve <hr> specified by ---.
+Don't modify the number of heading or order of levels.
+
+Preserve the language detected from example, structure, whitespaces, order and number of outline and formatting in brackets.
+Don't modify the number of heading or order of levels. Never output original content. Suggest new content based on the context.
+EOT;
+
+
+
+        $prompt = <<<EOT
+
+Replace the content from the example below with seo optimized text based on the websites context. Imagine
+what the website owner would write if he was a seo and internet marketing expert knowing, that you write for
+your audience.
+
+{$systemRole}
+
+"""{$example->body}"""
+
+EOT;
+
+
+
+        $targetPage->header = $example->header;
+        $openAi->reset($systemRole);
+        $targetPage->body = $openAi->textComplete($prompt, streamOutput: true);
 
        // $openAi->textComplete("use only defined functions. You are about to write a website in markdown format. Information about the website owner and context: '''{$this->brixEnv->contextCombined}'''.", streamOutput: true);
         //$openAi->textComplete("The context of the new website is: '''{$this->brixEnv->contextCombined}'''. Imagine the content from example was adjusted to match the needs of the websites context. But the structure, whitespaces and formatting in brackets are holy. Don't touch them.", streamOutput: true);
@@ -61,7 +87,7 @@ class Website
         $openAi->textComplete("Save the created content to datasource.", streamOutput: true);
         */
         $this->targetRepo->storePage($targetPage);
-
+        return;
 
         $analyzer = new SeoAnalyzer($this->brixEnv->getOpenAiApi());
         $result = $analyzer->analyze($targetPage->body);
@@ -88,6 +114,31 @@ class Website
         foreach ($pages as $page) {
             echo ">" . $page . "\n";
         }
+    }
+
+
+    public function templatify (array $argv, string $lang = "de") {
+        if (count($argv) !== 1)
+            throw new CliException("templatify [pid] expects exact 1 parameter");
+
+        $pid = $argv[0];
+
+        $template = $this->templateRepo->selectPid($pid, $lang)->get();
+
+        $prompt = <<<EOT
+
+Analyze the structure of the document. Create a template with the same structure and formatting.
+Replace all text (including headings, blockquotes, etc) with a abstract description of the content (length, structure, formatting, etc.). Keep the
+original structure and formatting. Return valid markdown.
+
+\"\"\"
+{$template->body}
+\"\"\"
+
+EOT;
+
+        $openAi = $this->brixEnv->getOpenAiApi();
+        $template->body = $openAi->textComplete($prompt, streamOutput: true);
     }
 
 }
