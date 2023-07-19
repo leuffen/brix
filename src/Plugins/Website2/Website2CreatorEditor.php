e6659c3ebc07bbf1683fe3f1354d93d430f434bf
Matthias Leuffen <matthes@leuffen.de>
2023-07-19 19:09:14 +0200

    add_temp

--- src/Plugins/Website2/Website2CreatorEditor.php
+++ src/Plugins/Website2/Website2CreatorEditor.php
@@ -9,6 +9,7 @@ use Lack\OpenAi\Helper\JobTemplate;
 use Lack\OpenAi\LackOpenAiClient;
 use Lack\OpenAi\LackOpenAiResponse;
 use Leuffen\Brix\Functions\SingleFileAccessFunctions;
+use Leuffen\Brix\Plugins\Seo\SeoAnalyzer;
 
 class Website2CreatorEditor
 {
@@ -40,16 +41,37 @@ class Website2CreatorEditor
         $tpl->setData([
             "context" => $this->context,
         ]);
-        $page = $this->targetRepo->selectPid($pid, $lang)->get();
+        $pagePid = $this->targetRepo->selectPid($pid, $lang);
+        if ($pagePid->hasTmp()) {
+            $page = $pagePid->getTmp();
+        } else {
+            $page = $pagePid->get();
+            $pagePid->setTmp($page);
+        }
+       
         $this->client->reset($tpl->getSystemContent());
+        $this->client->getCache()->clear();
         $this->client->textComplete([
             $page->body,
             $tpl->getUserContent()
         ], streamer: function (LackOpenAiResponse $response) use ($page) {
             $page->body = $response->getTextCleaned();
             $this->targetRepo->storePage($page);
-        });
+        });  
+        
+        $ret = (new SeoAnalyzer($this->client))->analyze($page->body);
+        $page->header["description"] = $ret->metaDescription;
+        $page->header["title"] = $ret->title;
+        $page->header["keywords"] = implode(", ", $ret->keywords);
+        $this->targetRepo->storePage($page);
+        
+        // Remove Temp file
+        $pagePid->setTmp(null);
+
+      
+        
     }
+    
     
   
 
